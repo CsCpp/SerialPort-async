99ec9143d8a017ea30b0c733c65e7a0e98850158
CppKotlin <cscppkotlin@gmail.com>
2024-09-29 21:57:02 +0300

    Добавьте файлы проекта.

---
+++ SerialPort/FormMy/Form2ComSendIn.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using static Mysqlx.Expect.Open.Types.Condition.Types;
+using System.Runtime.InteropServices;
+
+
+
+namespace SerialPortC
+{
+    public partial class Form2ComSendIn : Form
+    {
+        StreamWriter streamWriter;
+        string pathFile = @"C:\1.txt";
+
+        public Form5Grafika form5Grafika;
+
+        public Form1ComSet form1;
+        public Form3MySqlDATA objForm3;
+
+        public Form2ComSendIn()
+        {
+            InitializeComponent();
+        }
+        
+        public Form2ComSendIn(Form1ComSet f)
+        {
+            InitializeComponent();
+            form1 = f;
+           
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            form1.Visible = false;
+            saveMySQLToolStripMenuItem.Checked = false;
+            this.Text = "Терминал "+ form1.ComPortName();
+            addForm3Objct();
+           // form5Grafika = new Form5Grafika(form1.ComPortName());
+        }
+
+        private void Form2_FormClosed(object sender, FormClosedEventArgs e)
+        {
+          //form1.Close();
+            form1.Visible = true;
+            form1.ComPortClose();
+        }
+        
+        public void FormUpdate(string str)
+        {
+            sortData(str);
+           tBoxDataIN.Text += str;
+            ShowReloadForm3();
+            try
+            {
+                streamWriter = new StreamWriter(pathFile, true);
+                streamWriter.WriteLine(str);
+                streamWriter.Close();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
+        }
+
+        private void comPortToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (form1.Visible == true) { form1.Visible = false; }
+            else { form1.Visible = true; }
+        }
+
+        private void закрытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        //----------------------ТЕКСТ БОКС------------------------
+
+        private void btnClearData_Click(object sender, EventArgs e)
+        {
+            tBoxDataIN.Text = "";
+            tBoxDataOut.Text = "";
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            sendData();
+        }
+
+        private void tBoxDataOut_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                sendData();
+            }
+        }
+
+        private async Task sendData()
+        {
+           await form1.sendDataEnter(tBoxDataOut.Text);
+            addForm3Objct();
+            ShowReloadForm3();
+            tBoxDataOut.Text = "";
+        }
+
+        //----------------------Показать БАЗУ ДАННЫХ------------------------
+
+        private void showDataToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            addForm3Objct();
+            objForm3.Show();
+        }
+
+        public void addForm3Objct()
+        {
+            if (Form3MySqlDATA.activForm3Status == false)
+            {
+                objForm3 = new Form3MySqlDATA(form1.ComPortName());
+            }
+           
+        }
+        //----------------------Обновить БАЗУ ДАННЫХ------------------------
+
+        public void ShowReloadForm3()
+        {
+            addForm3Objct();
+            objForm3.RefreshAndShowDataOnDataGidView();
+            
+        }
+        //----------------------Вкл. обманку данных -------------------------
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+             new Thread(() => randomZUmanka()).Start();
+            }
+        }
+
+        private void randomZUmanka()
+        {
+            Random random = new Random();
+            while (true)
+            {
+                if (checkBox1.Checked)
+                {
+                    try
+                    {
+                        form1.sendDataEnter(Convert.ToString($"I={random.Next(1, 19)}A U={random.Next(9, 16)}V \n"));
+                    }
+                    catch (Exception ex)
+                    {
+
+                        MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+
+                         Thread.Sleep(1000);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+        //-----------------------Сортировка----------------------------------
+        private  void sortData(string str)
+        {
+            int indexOfI = str.LastIndexOf("I=") + 2;
+            int indexOfU = str.LastIndexOf("U=") + 2;
+            string tempI = "";
+            string tempU = "";
+            int varI = 0;
+            int varU = 0;
+
+            for (int i = indexOfI; i < str.Length; i++)
+            {
+                if (str[i] == 'A') break;
+                tempI += str[i];
+            }
+            for (int i = indexOfU; i < str.Length; i++)
+            {
+                if (str[i] == 'V') break;
+                tempU += str[i];
+            }
+            try
+            {
+                varI = Convert.ToInt32(tempI);
+                varU = Convert.ToInt32(tempU);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            // Console.WriteLine("Ток = " + varI + "А ----- Напряжение U = " + varU + "В");
+
+
+            //form5Grafika ??= new Form5Grafika(form1.ComPortName()); для 8 С#
+
+            if (form5Grafika == null)
+            {
+                form5Grafika=  new Form5Grafika(form1.ComPortName());
+                form5Grafika.FormClosing += onForm5Closed; 
+            }
+            
+            
+            form5Grafika.dataIU(varI, varU);
+            form5Grafika.Show();
+        }
+        
+        private void onForm5Closed(object sender, FormClosingEventArgs e)
+            {
+                form5Grafika.FormClosing -= onForm5Closed;
+                form5Grafika = null;
+            }
+
+    }
+}
---
+++ SerialPort/FormMy/Form5Grafika.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace SerialPortC
+
+{
+    partial class Form5Grafika
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label2I = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label4U = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label2I
+            // 
+            this.label2I.AutoSize = true;
+            this.label2I.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2I.Location = new System.Drawing.Point(8, 20);
+            this.label2I.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2I.Name = "label2I";
+            this.label2I.Size = new System.Drawing.Size(84, 42);
+            this.label2I.TabIndex = 1;
+            this.label2I.Text = "000";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label4U);
+            this.groupBox2.ForeColor = System.Drawing.Color.SteelBlue;
+            this.groupBox2.Location = new System.Drawing.Point(185, 4);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Size = new System.Drawing.Size(162, 75);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            // 
+            // label4U
+            // 
+            this.label4U.AutoSize = true;
+            this.label4U.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4U.Location = new System.Drawing.Point(8, 20);
+            this.label4U.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4U.Name = "label4U";
+            this.label4U.Size = new System.Drawing.Size(84, 42);
+            this.label4U.TabIndex = 1;
+            this.label4U.Text = "000";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Controls.Add(this.chart1);
+            this.groupBox3.Location = new System.Drawing.Point(15, 86);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox3.Size = new System.Drawing.Size(468, 274);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "I/U";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Gray;
+            this.label1.Location = new System.Drawing.Point(118, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // chart1
+            // 
+            this.chart1.BackColor = System.Drawing.Color.Black;
+            this.chart1.BorderlineColor = System.Drawing.Color.Black;
+            this.chart1.BorderSkin.BorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dash;
+            chartArea1.AxisX.LabelStyle.ForeColor = System.Drawing.Color.Gray;
+            chartArea1.AxisX.LineColor = System.Drawing.Color.Gray;
+            chartArea1.AxisX.MajorGrid.LineColor = System.Drawing.Color.Gray;
+            chartArea1.AxisX.MajorTickMark.LineColor = System.Drawing.Color.Gray;
+            chartArea1.AxisX.MinorGrid.LineColor = System.Drawing.Color.Gray;
+            chartArea1.AxisX.TitleForeColor = System.Drawing.Color.DarkGray;
+            chartArea1.AxisX2.LabelStyle.ForeColor = System.Drawing.Color.Gray;
+            chartArea1.AxisX2.LineColor = System.Drawing.Color.Gray;
+            chartArea1.AxisX2.MajorGrid.LineColor = System.Drawing.Color.Gray;
+            chartArea1.AxisX2.MajorTickMark.LineColor = System.Drawing.Color.Gray;
+            chartArea1.AxisX2.MinorGrid.LineColor = System.Drawing.Color.Gray;
+            chartArea1.AxisX2.MinorTickMark.LineColor = System.Drawing.Color.Gray;
+            chartArea1.AxisX2.ScaleBreakStyle.LineColor = System.Drawing.Color.Gray;
+            chartArea1.AxisX2.TitleForeColor = System.Drawing.Color.Gray;
+            chartArea1.AxisY.LabelStyle.ForeColor = System.Drawing.Color.Gray;
+            chartArea1.AxisY.LineColor = System.Drawing.Color.Gray;
+            chartArea1.AxisY.MajorGrid.LineColor = System.Drawing.Color.Gray;
+            chartArea1.AxisY.TitleForeColor = System.Drawing.Color.Gray;
+            chartArea1.AxisY2.LineColor = System.Drawing.Color.DarkGray;
+            chartArea1.AxisY2.TitleForeColor = System.Drawing.Color.Gray;
+            chartArea1.BackColor = System.Drawing.Color.Black;
+            chartArea1.BorderColor = System.Drawing.Color.Gray;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chart1.ImeMode = System.Windows.Forms.ImeMode.AlphaFull;
+            this.chart1.Location = new System.Drawing.Point(4, 19);
+            this.chart1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.SemiTransparent;
+            series1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            series1.BorderWidth = 2;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Color = System.Drawing.Color.Red;
+            series1.EmptyPointStyle.Color = System.Drawing.Color.Red;
+            series1.IsVisibleInLegend = false;
+            series1.LabelBorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dot;
+            series1.Legend = "Legend1";
+            series1.MarkerBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            series1.MarkerColor = System.Drawing.Color.Red;
+            series1.MarkerSize = 4;
+            series1.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Triangle;
+            series1.Name = "I";
+            series2.BorderWidth = 2;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Color = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            series2.IsVisibleInLegend = false;
+            series2.Legend = "Legend1";
+            series2.MarkerBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            series2.MarkerBorderWidth = 2;
+            series2.MarkerColor = System.Drawing.Color.Blue;
+            series2.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Circle;
+            series2.Name = "U";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(460, 251);
+            this.chart1.TabIndex = 0;
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.comboBox1.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "10 sek",
+            "1 min",
+            "10 min",
+            "30 min ",
+            "1 hour",
+            "1 day"});
+            this.comboBox1.Location = new System.Drawing.Point(361, 39);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(92, 24);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2I);
+            this.groupBox1.ForeColor = System.Drawing.Color.DarkRed;
+            this.groupBox1.Location = new System.Drawing.Point(15, 4);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(162, 75);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // Form5Grafika
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(494, 375);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MinimumSize = new System.Drawing.Size(489, 363);
+            this.Name = "Form5Grafika";
+            this.Text = "VoltAmpetr";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form5Grafika_FormClosed);
+            this.Load += new System.EventHandler(this.Form5Grafika_Load);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2I;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label4U;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
